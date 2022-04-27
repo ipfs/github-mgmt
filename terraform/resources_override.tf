b337b167401cd58320ab5ba81828e72b15be4590
galargh <piotr.galar@gmail.com>
2022-04-27 11:45:03 +0200

    setup: repository

--- terraform/resources_override.tf
+++ terraform/resources_override.tf
@@ -1,24 +1,62 @@
-resource "github_branch_protection" "this" {
+resource "github_membership" "this" {
   lifecycle {
-    # @resources.branch_protection.ignore_changes
+    # @resources.membership.ignore_changes
     ignore_changes = [
-      allows_deletions,
-      allows_force_pushes,
-      enforce_admins,
+      etag,
       id,
-      push_restrictions,
-      require_conversation_resolution,
-      require_signed_commits,
-      required_linear_history,
-      # required_pull_request_reviews,
-      required_pull_request_reviews[0].dismiss_stale_reviews,
-      required_pull_request_reviews[0].dismissal_restrictions,
-      required_pull_request_reviews[0].require_code_owner_reviews,
-      required_pull_request_reviews[0].required_approving_review_count,
-      required_pull_request_reviews[0].restrict_dismissals,
-      # required_status_checks,
-      # required_status_checks[0].contexts,
-      # required_status_checks[0].strict
+      # role
+    ]
+  }
+}
+
+resource "github_repository_collaborator" "this" {
+  lifecycle {
+    # @resources.repository_collaborator.ignore_changes
+    ignore_changes = [
+      id,
+      invitation_id,
+      # permission,
+      permission_diff_suppression
+    ]
+  }
+}
+
+resource "github_team" "this" {
+  lifecycle {
+    # @resources.team.ignore_changes
+    ignore_changes = [
+      # id,
+      create_default_maintainer,
+      description,
+      etag,
+      ldap_dn,
+      members_count,
+      node_id,
+      # parent_team_id,
+      # privacy,
+      slug
+    ]
+  }
+}
+
+resource "github_team_repository" "this" {
+  lifecycle {
+    # @resources.team_repository.ignore_changes
+    ignore_changes = [
+      etag,
+      id,
+      # permission
+    ]
+  }
+}
+
+resource "github_team_membership" "this" {
+  lifecycle {
+    # @resources.team_membership.ignore_changes
+    ignore_changes = [
+      etag,
+      id,
+      # role
     ]
   }
 }
