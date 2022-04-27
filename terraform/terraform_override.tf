83ab2b6afebefbc6eff56ab7b3a908876c6878f4
galargh <piotr.galar@gmail.com>
2022-04-27 10:23:30 +0200

    setup: AWS

--- terraform/terraform_override.tf
+++ terraform/terraform_override.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "s3" {
+    # account_id = "642361402189"
     region               = "us-east-1"
     bucket               = "github-mgmt"
     key                  = "terraform.tfstate"
