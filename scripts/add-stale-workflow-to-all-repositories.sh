6768fbae84651c7ec0a804b0e37cb13a71240a47
galargh <piotr.galar@gmail.com>
2022-05-10 12:41:22 +0200

    add need/author-input label and stale workflow where needed

--- scripts/add-stale-workflow-to-all-repositories.sh
+++ scripts/add-stale-workflow-to-all-repositories.sh
@@ -9,19 +9,21 @@ root="$(realpath "$(dirname "$0")/..")"
 repository_file="$(jq '.' "$root/github/ipfs/repository_file.json")"
 
 while read repository; do
-  echo "Checking if $repository has stale.yml already"
+  echo "Checking if $repository has stale worlflow already"
   has_stale="$(jq '.[$repository] | .[".github/workflows/stale.yml"] | . != null' --arg repository "$repository" <<< "$repository_file")"
   if [[ "$has_stale" == 'true' ]]; then
-    echo "stale.yml already exists in $repository, skipping"
+    echo "Stale workflow already exists in $repository, skipping"
   else
-    echo "Checking if $repository has either needs/author-input or hints/needs-author-input label"
-    if gh api repos/ipfs/$repository/labels/need/author-input > /dev/null 2>&1 || gh api repos/ipfs/$repository/labels/hint/needs-author-input > /dev/null 2>&1; then
-      echo "Found label in $repository, adding stale.yml"
+    echo "Checking if $repository has need/author-input label"
+    if gh api repos/ipfs/$repository/labels/need/author-input > /dev/null 2>&1; then
+      echo "Found label in $repository, adding stale workflow"
       repository_file="$(jq '.[$repository] +=  { ".github/workflows/stale.yml": { "content": "./.github/workflows/stale.yml" } }' --arg repository "$repository" <<< "$repository_file")"
     else
       echo "No label found in $repository, skipping"
     fi
   fi
+
+  echo $repository
 done <<< "$(jq -r 'keys | .[]' "$root/github/ipfs/repository.json")"
 
 echo "Saving new repository_file configuration"
