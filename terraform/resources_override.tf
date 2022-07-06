4f6631a66aac761bbe33808a1c63b0dc2e3ab043
galargh <piotr.galar@gmail.com>
2022-07-06 16:26:48 +0200

    chore: manual steps required by the upgrade

--- terraform/resources_override.tf
+++ terraform/resources_override.tf
@@ -1,79 +1,56 @@
-resource "github_membership" "this" {
+resource "github_team" "this" {
   lifecycle {
-    # @resources.membership.ignore_changes
+    ignore_changes = []
+  }
+}
+
+resource "github_repository" "this" {
+  lifecycle {
     ignore_changes = [
-      etag,
-      id,
-      # role
+      allow_auto_merge,
+      allow_merge_commit,
+      allow_rebase_merge,
+      allow_squash_merge,
+      archive_on_destroy,
+      archived,
+      auto_init,
+      delete_branch_on_merge,
+      gitignore_template,
+      has_downloads,
+      has_issues,
+      has_projects,
+      has_wiki,
+      homepage_url,
+      ignore_vulnerability_alerts_during_read,
+      is_template,
+      license_template,
+      pages,
+      template,
+      vulnerability_alerts,
     ]
   }
 }
 
-resource "github_repository_collaborator" "this" {
+resource "github_branch_protection" "this" {
   lifecycle {
-    # @resources.repository_collaborator.ignore_changes
     ignore_changes = [
-      id,
-      invitation_id,
-      # permission,
-      permission_diff_suppression
+      allows_deletions,
+      allows_force_pushes,
+      enforce_admins,
+      push_restrictions,
+      require_conversation_resolution,
+      require_signed_commits,
+      required_linear_history,
+      required_pull_request_reviews,
+      required_status_checks,
     ]
   }
 }
 
 resource "github_repository_file" "this" {
   lifecycle {
-    # @resources.repository_file.ignore_changes
     ignore_changes = [
-      id,
-      # branch,
-      commit_author,
-      commit_email,
-      commit_message,
-      commit_sha,
       overwrite_on_create,
-      sha
-    ]
-  }
-}
-
-
-resource "github_team" "this" {
-  lifecycle {
-    # @resources.team.ignore_changes
-    ignore_changes = [
-      # id,
-      create_default_maintainer,
-      description,
-      etag,
-      ldap_dn,
-      members_count,
-      node_id,
-      # parent_team_id,
-      # privacy,
-      slug
-    ]
-  }
-}
-
-resource "github_team_repository" "this" {
-  lifecycle {
-    # @resources.team_repository.ignore_changes
-    ignore_changes = [
-      etag,
-      id,
-      # permission
-    ]
-  }
-}
-
-resource "github_team_membership" "this" {
-  lifecycle {
-    # @resources.team_membership.ignore_changes
-    ignore_changes = [
-      etag,
-      id,
-      # role
     ]
   }
 }
