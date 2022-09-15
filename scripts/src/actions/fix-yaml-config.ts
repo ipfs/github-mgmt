cf600119296934dd4a06d7fa04f6da6f510d1d36
galargh <piotr.galar@gmail.com>
2022-09-15 19:10:18 +0200

    feat: automate adding stale.yml to all repos

--- scripts/src/actions/fix-yaml-config.ts
+++ scripts/src/actions/fix-yaml-config.ts
@@ -1,4 +1,6 @@
 import 'reflect-metadata'
+import {addFileToAllRepos} from './shared/add-file-to-all-repos'
 import {format} from './shared/format'
 
+addFileToAllRepos('.github/workflows/stale.yml', '.github/workflows/stale.yml')
 format()
