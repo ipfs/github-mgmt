269b0df3c1af55e714b5589c79f3ecc773b958c5
galargh <piotr.galar@gmail.com>
2022-09-15 09:49:46 +0200

    refactor: use add file to all repos with filter instead of a custom function

--- scripts/src/actions/fix-yaml-config.ts
+++ scripts/src/actions/fix-yaml-config.ts
@@ -1,6 +1,12 @@
 import 'reflect-metadata'
-import { addFileToGUIRepos } from './shared/add-file-to-gui-repos'
+import { Repository } from '../resources/repository'
+import { addFileToAllRepos } from './shared/add-file-to-all-repos'
 import {format} from './shared/format'
 
-addFileToGUIRepos()
+function isGUIRepository(repository: Repository): boolean {
+  const GUI_TOPICS = ['ipfs-gui', 'gui']
+  return repository.topics?.some(t => GUI_TOPICS.includes(t)) ?? false
+}
+
+addFileToAllRepos('.github/workflows/add_issues_to_gui_project.yml', '.github/workflows/add_issues_to_gui_project.yml', isGUIRepository)
 format()
