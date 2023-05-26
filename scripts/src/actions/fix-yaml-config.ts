a12296a22122f5af11750e7e52a1b916d35f5de5
Piotr Galar <piotr.galar@gmail.com>
2023-05-26 14:12:37 +0200

    Update fix-yaml-config.ts

--- scripts/src/actions/fix-yaml-config.ts
+++ scripts/src/actions/fix-yaml-config.ts
@@ -2,17 +2,36 @@ import 'reflect-metadata'
 import {Repository} from '../resources/repository'
 import {addFileToAllRepos} from './shared/add-file-to-all-repos'
 import {format} from './shared/format'
+import {protectDefaultBranches} from './shared/protect-default-branches'
+import {setPropertyInAllRepos} from './shared/set-property-in-all-repos'
 
-const uninitialisedRepositoryNames = [
-  '2022.ipfs.camp',
-  'go-data-transfer-bus',
-  'lightning-storm',
-  'helia-ipns'
-]
+function isInitialised(repository: Repository) {
+  return ![
+    '2022.ipfs.camp',
+    'go-data-transfer-bus',
+    'lightning-storm',
+    'helia-ipns'
+  ].includes(repository.name)
+}
 
+function isPublic(repository: Repository) {
+  return repository.visibility === 'public'
+}
+
+protectDefaultBranches()
 addFileToAllRepos(
-  '.github/workflows/stale.yml', 
   '.github/workflows/stale.yml',
-  (repository: Repository) => !uninitialisedRepositoryNames.includes(repository.name)
+  '.github/workflows/stale.yml',
+  isInitialised
+)
+setPropertyInAllRepos(
+  'secret_scanning',
+  true,
+  r => isInitialised(r) && isPublic(r)
+)
+setPropertyInAllRepos(
+  'secret_scanning_push_protection',
+  true,
+  r => isInitialised(r) && isPublic(r)
 )
 format()
