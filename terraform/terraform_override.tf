74d0d475ac761b3d5769b6ea30a077c2d7008dd4
Piotr Galar <piotr.galar@gmail.com>
2025-04-20 13:13:43 +0200

    Update terraform_override.tf

--- terraform/terraform_override.tf
+++ terraform/terraform_override.tf
@@ -2,9 +2,9 @@ terraform {
   backend "s3" {
     # account_id = < Managed by https://ipdx.co >
     region               = "us-east-1"
-    bucket               = "github-as-code-interplanetary-shipyard"
+    bucket               = "github-as-code-v1-ipshipyard"
     key                  = "terraform.tfstate"
     workspace_key_prefix = "org"
-    dynamodb_table       = "github-as-code-interplanetary-shipyard"
+    dynamodb_table       = "github-as-code-v1-ipshipyard"
   }
 }
